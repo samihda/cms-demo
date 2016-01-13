c0fe6fa6de5c8c20d53ba8eaedd55580be51a9d3
samihda <samihda@users.noreply.github.com>
2016-01-13 08:08:29 +0100

    new database schema

--- db/users.js
+++ db/users.js
@@ -1,18 +1,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/cms-test', function (err) {
+mongoose.connect('mongodb://localhost/cms-test-local2', function (err) {
   if (err) {
     console.log('database connection error ' + err);
   }
-  else {
-    console.log('connected to mongodb');
-  }
+  console.log('connected to mongodb');
 });
 
 var ArticleSchema = new Schema({
   title: String,
   body: String,
+  author_id: String,
   date: {
     type: Date,
     default: Date.now
@@ -23,8 +22,7 @@ var Article = mongoose.model('Article', ArticleSchema);
 var UserSchema = new Schema({
   username: String,
   password: String,
-  email: String,
-  articles: [ArticleSchema]
+  email: String
 });
 var User = mongoose.model('User', UserSchema);
 
@@ -36,12 +34,9 @@ exports.findById = function(id, cb) {
   process.nextTick(function() {
     User.findOne({_id: id}, {password: 0}, function (err, user) {
       if (err) {
-        //console.log(err);
         cb(new Error('The user does not exist'));
       }
-      else {
-        cb(null, user);
-      }
+      cb(null, user);
     });
   });
 };
@@ -53,9 +48,7 @@ exports.findByUsername = function(name, cb) {
       if (err) {
         return cb(null, null);
       }
-      else {
-        return cb(null, user);
-      }
+      return cb(null, user);
     });
   });
 };
