800765daa8b2fafcee8c4d5d12a468a2cc7e691a
samihda <samihda@users.noreply.github.com>
2016-01-13 08:17:23 +0100

    database name fix

--- db/users.js
+++ db/users.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/cms-test-local2', function (err) {
+mongoose.connect('mongodb://localhost/cms-test', function (err) {
   if (err) {
     console.log('database connection error ' + err);
   }
