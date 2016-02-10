4f1334be66e281fedc323d3e8bca31eea7c09a49
samihda <samihda@users.noreply.github.com>
2016-02-10 01:45:55 +0100

    dummy home page

---
+++ public/app/home.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	template: `<h1>Home page</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.</p>`
+})
+export class HomeComponent {}
